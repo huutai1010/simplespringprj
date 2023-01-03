b7c706a62295201a00438df5957930a5c4a448f9
huutai1010 <huutaia21010@gmail.com>
2023-01-03 15:16:44 +0700

    Add api

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,11 +3,18 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HomeController {
-    
-    @GetMapping("/")
-    public String home() {
-        return "Hello from Azure App Service (in the staging slot)!";
+
+    @GetMapping("/get-data")
+    public Map<String, String> sayHello() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        return map;
     }
 }
